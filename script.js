0d01f25c23d23e5c5f341a810c55789c644cb01b
CodePredator01 <priyanshu.vishwakarma_cs19@gla.ac.in>
2021-11-25 17:24:18 -0600

    final commit

--- script.js
+++ script.js
@@ -91,3 +91,97 @@ document.getElementById('leave-btn').addEventListener('click', async () => {
 
 
 
+
+//Method will take all my info and set user stream in frame
+let joinStreams = async () => {
+    //Is this place hear strategicly or can I add to end of method?
+    
+    client.on("user-published", handleUserJoined);
+    client.on("user-left", handleUserLeft);
+
+
+    client.enableAudioVolumeIndicator(); // Triggers the "volume-indicator" callback event every two seconds.
+    client.on("volume-indicator", function(evt){
+        for (let i = 0; evt.length > i; i++){
+            let speaker = evt[i].uid
+            let volume = evt[i].level
+            if(volume > 0){
+                document.getElementById(`volume-${speaker}`).src = './assets/volume-on.svg'
+            }else{
+                document.getElementById(`volume-${speaker}`).src = './assets/volume-off.svg'
+            }
+            
+        
+            
+        }
+    });
+
+    //#6 - Set and get back tracks for local user
+    [config.uid, localTracks.audioTrack, localTracks.videoTrack] = await  Promise.all([
+        client.join(config.appid, config.channel, config.token ||null, config.uid ||null),
+        AgoraRTC.createMicrophoneAudioTrack(),
+        AgoraRTC.createCameraVideoTrack()
+
+    ])
+    
+    //#7 - Create player and add it to player list++
+    let player = `<div class="video-containers" id="video-wrapper-${config.uid}">
+                        <p class="user-uid"><img class="volume-icon" id="volume-${config.uid}" src="./assets/volume-on.svg" /> ${config.uid}</p>
+                        <div class="video-player player" id="stream-${config.uid}"></div>
+                  </div>`
+
+    document.getElementById('user-streams').insertAdjacentHTML('beforeend', player);
+    //#8 - Player user stream in div++
+    localTracks.videoTrack.play(`stream-${config.uid}`)
+    
+
+    //#9 Add user to user list of names/ids
+
+    //#10 - Publish my local video tracks to entire channel so everyone can see it
+    await client.publish([localTracks.audioTrack, localTracks.videoTrack])
+
+}
+
+
+let handleUserJoined = async (user, mediaType) => {
+    console.log('Handle user joined')
+
+    //#11 - Add user to list of remote users
+    remoteTracks[user.uid] = user
+
+    //#12 Subscribe ro remote users
+    await client.subscribe(user, mediaType)
+   
+    
+    if (mediaType === 'video'){
+        let player = document.getElementById(`video-wrapper-${user.uid}`)
+        console.log('player:', player)
+        if (player != null){
+            player.remove()
+        }
+ 
+        player = `<div class="video-containers" id="video-wrapper-${user.uid}">
+                        <p class="user-uid"><img class="volume-icon" id="volume-${user.uid}" src="./assets/volume-on.svg" /> ${user.uid}</p>
+                        <div  class="video-player player" id="stream-${user.uid}"></div>
+                      </div>`
+        document.getElementById('user-streams').insertAdjacentHTML('beforeend', player);
+         user.videoTrack.play(`stream-${user.uid}`)
+
+        
+
+          
+    }
+    
+
+    if (mediaType === 'audio') {
+        user.audioTrack.play();
+      }
+}
+
+
+let handleUserLeft = (user) => {
+    console.log('Handle user left!')
+    //Remove from remote users and remove users video wrapper
+    delete remoteTracks[user.uid]
+    document.getElementById(`video-wrapper-${user.uid}`).remove()
+}
